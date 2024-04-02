821e3a14505066359bb3854b4c48361023275652
74716 <ndeb2450@gmail.com>
2024-04-02 23:43:41 +0530

    Basic Components Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import SideBar from './components/SideBar';
+import CreatePost from './components/CreatePost';
+import Post from './components/Post';
 
 function App() {
  
 
   return (
     <>
-      <div>Placeholder</div>
+    <div className='app-container'>
+      <SideBar></SideBar>
+      <div className='content'>
+        <Header></Header> 
+        <CreatePost></CreatePost>
+        <Post></Post>
+        <Footer></Footer>
+      </div>
+    </div>
     </>
   )
 }
