5c879821b4fee0214ea08fbf4fe65a54b1744a2a
74716 <ndeb2450@gmail.com>
2024-04-04 00:04:27 +0530

    delete post functionality working

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,18 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useContext } from "react";
+import { MdOutlinePlaylistRemove } from "react-icons/md";
+import { PostList } from "../store/postList-store";
 
 function Post({post}){
+    const{deletePost} = useContext(PostList) //from PostList context I destructured deletePost method here
+
     return(
-        <div className="card post-card" style = {{width: "18rem"}}>
+        <div className="card post-card" style = {{width: "30rem"}}>
             <div className="card-body">
-            <span class="badge rounded-pill text-bg-dark user">{post.userID}</span>
+            <span className="badge rounded-pill text-bg-dark user">{post.userID}</span>
                 <h5 className="card-title">{post.title}
-                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                    {post.reactions}
-                    <span class="visually-hidden">Reactions</span>
+                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+                  onClick={()=> deletePost(post.id)}>
+                <MdOutlinePlaylistRemove />
+                    <span className="visually-hidden">Delete</span>
                 </span>
                 </h5>
                 <p className="card-text">{post.body}</p>
-                {post.tags.map((tag)=>(<span class="badge text-bg-light">{`#${tag}`}</span>))}
+                {post.tags.map((tag)=>(<span key={tag} className="badge text-bg-light">{`#${tag}`}</span>))}
+                <div className="alert alert-info" role="alert">
+                    {post.reactions} people have reacted on your post
+                </div>
             </div>
         </div>
     )
--- src/store/postList-store.jsx
+++ src/store/postList-store.jsx
@@ -10,7 +10,15 @@ export const PostList = createContext(
 
 
 const postListReducer = (currPostList, action) =>{ //reducer function
-    return currPostList;
+    
+    let newPostList = currPostList;
+    console.log(newPostList)
+    if(action.type === "DELETE_POST"){
+       //baapreh.. dont put {} after (post)=> (why that i dont know yet) 
+        newPostList = currPostList.filter((post)=> post.id !== action.payload.postId);
+    }
+    console.log(newPostList)
+    return newPostList;
 }
 
 //PostListProvider is a component which wraps children inside <PostList.Provider /> and renders them
@@ -18,7 +26,7 @@ const PostListProvider = ({children}) => {
 
     const DEFAULT_POST_LIST =  [
                                     {
-                                        id:"1",
+                                        id: 1,
                                         title:"Going to Mayapur",
                                         body:"Finally Able to go to Mayapur woww!! All Glories to Vaishnavs and Gurudev",
                                         reactions: 32,
@@ -27,26 +35,34 @@ const PostListProvider = ({children}) => {
                                     },
 
                                     {
-                                        id:"2",
+                                        id: 2,
                                         title:"Studing for Placement",
                                         body:"Studing React right now need to revice the already done subjects like JAVA, CPP, C, Java Script",
                                         reactions: 64,
                                         userID: "jash_64",
                                         tags: ['Grinding', 'focus']
                                     }
-                               ]
+                               ];
 
-    const[postList, dispatchPostList] = useReducer(postListReducer, DEFAULT_POST_LIST) 
+    const[postList, dispatchPostList] = useReducer(postListReducer, DEFAULT_POST_LIST) ;
     //inital values assigned to postlist array is DEFAULT_POST_LIST 
     //intial postList is passed here which will be updated and dispatched depending on action assigned to postListReducer()
 
     const addPost = () =>{
-
+       
     }
     
-    const deletePost = () =>{
-    
-    }
+    const deletePost = (postId) =>{
+        //console.log(`delete post called for ${postId}`);
+        dispatchPostList(
+            {
+                type: "DELETE_POST",
+                payload: {
+                    postId,
+                },
+            }
+        );
+    };
 
     return (
         <PostList.Provider value={ { //the object is passed in ContextProvider so that all children component can use them
