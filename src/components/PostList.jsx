f1a650805b49d6333ed834472786584e916f63de
74716 <ndeb2450@gmail.com>
2024-04-04 20:45:50 +0530

    using useState we are loading the initial posts

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Post from "./Post";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import {PostList as PostListData} from "../store/postList-store"
 import EmptyMessage from "./EmptyMessage";
 
@@ -10,11 +10,23 @@ function PostList(){
     {/*We have destructured the postList (array) in the objects of PostListData to be used here so that we 
        can create individual posts using the array which is being passed from the store using map method*/}
 
+    {/*here we wish to have dummy data as soon as the app is being painted for the firs time and should'nt repaint 
+       entire dummy array again and again on any changes, therefore, the fetch function will run once the app starts 
+       newly and after it runs once the dataFetched variable will be toggled to true .. next time on any change 
+       the UI wouldnt be repaeted painted with the posts array comming from dummy JSON */}
+    const [dataFetched, setDataFetched] =  useState(false); //initially the value of dataFetched is Kept false
+    if(!dataFetched){
+        fetch('https://dummyjson.com/posts')
+        .then(res => res.json())
+        .then((data)=>addInitialPost(data.posts));
+        setDataFetched(true);
+    }
+       
        const handleGetPostClicked =() =>{
             //console.log("Get Post Click clicked")
-            fetch('https://dummyjson.com/posts')
-            .then(res => res.json())
-            .then((data)=>addInitialPost(data.posts));
+            // fetch('https://dummyjson.com/posts')
+            // .then(res => res.json())
+            // .then((data)=>addInitialPost(data.posts));
        }
 
     return(
